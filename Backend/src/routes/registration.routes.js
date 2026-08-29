d8b088708eb3867b7bab342074798185f5e57ecc
Ch Abhiram <abhiramch018@gmail.com>
2026-08-29 16:11:56 +0530

    Fixed Registrations

---
+++ Backend/src/routes/registration.routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const router = express.Router();
+
+const registrationController =
+    require("../controllers/registration.controller");
+
+const authMiddleware =
+    require("../middlewares/auth.middleware");
+
+const authorize =
+    require("../middlewares/authorize.middleware");
+
+
+// REGISTER FOR ACTIVITY
+// Only normal users can register
+// Admin cannot register
+router.post(
+    "/",
+    authMiddleware,
+    authorize("user"),
+    registrationController.registerForActivity
+);
+
+
+module.exports = router;
