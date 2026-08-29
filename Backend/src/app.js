797664e5cac91b95715d232a920f15ace10a9b53
GajjelaVasudev <vgajjella@gmail.com>
2026-08-29 15:05:03 +0530

    Fixed OTP Verification

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,6 +6,8 @@ const app = express();
 
 const authRoutes = require('./routes/auth.routes');
 const userRoutes = require('./routes/user.routes');
+const adminUsersRoutes = require('./routes/adminUsers.routes');
+const badgeRoutes = require('./routes/badge.routes');
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -17,6 +19,9 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
+app.use('/api/admin/user', adminUsersRoutes);
+app.use('/api/badge', badgeRoutes);
+
 
 
 
